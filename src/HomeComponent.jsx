a05016fe6f4b427ed8ab813ab6dae6a2057099f8
alejoquintana <alejoquintana00@gmail.com>
2023-09-12 10:30:16 -0300

    images

--- src/HomeComponent.jsx
+++ src/HomeComponent.jsx
@@ -2,8 +2,8 @@ import "./home.scss"
 
 const HomeComponent = () => {
 	return (
-		<div className='section-container grid grid-cols-2 background' id='init'>
-			<div className='row-span-1 flex flex-col justify-center'>
+		<div className='section-container grid grid-cols-3 background' id='init'>
+			<div className='row-span-1 col-span-2 flex flex-col justify-center'>
 				<h1 className='text-8xl text-primary font-black'>Twister Futbol</h1>
 				<p className='text-dark'>
 					Empresa familiar dedicada a fabricar indumentaria deportiva con mas de 25 años de vida.
